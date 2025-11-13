207104dd6aa3aec345079388899dc27c601d3a06
Данило <@LAPTOP-7M1VSVI1>
2025-11-13 23:05:34 +0200

    Create project prototype.

---
+++ MyStdString/MyStdString.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <cstring>
+
+namespace my_std {
+    class MyString {
+    private:
+        char* ptr;
+        char* ptr_end;
+        char* ptr_end_capacity;
+    public:
+        MyString() {
+
+        }
+        ~MyString() {
+
+        }
+    };
+}
+
+int main()
+{
+    std::cout << "Hello World!\n" << std::endl;
+
+    return 0;
+}

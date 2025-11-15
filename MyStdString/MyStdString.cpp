dc78e74adc320a1ba69fc7bad95a3b35f17c9aef
Данило <@LAPTOP-7M1VSVI1>
2025-11-15 14:25:31 +0200

    Start project. WIP: Implementing string assignment operator.

--- MyStdString/MyStdString.cpp
+++ MyStdString/MyStdString.cpp
@@ -5,22 +5,69 @@
 namespace my_std {
     class MyString {
     private:
-        char* ptr;
-        char* ptr_end;
-        char* ptr_end_capacity;
+        char* str;
+        char* str_end;
+        char* str_end_capacity;
+        size_t str_size = 0;
+        size_t str_capacity;
     public:
         MyString() {
-
+            str_capacity = 32;
+            str = new char[str_capacity];
+            str_end = &str[str_size];
+            str_end_capacity = &str[str_capacity];
+            str[0] = '\0';
+        }
+        MyString(char* other_str) {
+            for (char* str_temp = other_str; *str_temp != '\0'; str_temp++) {
+                str_size++;
+            }
+            str_capacity = str_size * 1.5;
+            str = new char[str_capacity];
+            str_end = &str[str_size];
+            str_end_capacity = &str[str_capacity];
+            *str_end = '\0';
+            show_string();
         }
         ~MyString() {
-
+            delete[] str;
         }
+        MyString& operator=(const char* other) {
+            size_t other_size = 0;
+            for (const char* ptr = other; ptr != '\0'; ptr++) {
+                other_size++;
+            }
+            other_size++;
+            if (str) {
+                delete[] str;
+                str_size = other_size;
+                str_capacity = str_size * 1.5;
+                str = new char[str_capacity];
+                str_end = &str[str_size];
+                str_end_capacity = &str[str_capacity];
+                *str_end = '\0';
+            }
+            else {
+                char* str_temp = str;
+                for (; str_temp != '\0'; str_temp++) {
+                    *str_temp = *other;
+                }
+                *str_end = '\0';
+            }
+            return *this;
+        }
+        void show_string() {
+            std::cout << str << std::endl;
+        }
+
     };
 }
 
 int main()
 {
-    std::cout << "Hello World!\n" << std::endl;
+    my_std::MyString str;
+    str = "Hello World!";
+    str.show_string();
 
     return 0;
 }
